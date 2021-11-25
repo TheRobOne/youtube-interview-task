2b5318a396b8a4c4ee3302132c36cd2c4f669efa
TheRobOne <m_rusek@vp.pl>
2021-11-25 22:05:53 +0100

    Test YouTube api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=random&key=${process.env.REACT_APP_API_KEY}`;
+      const result = await axios.get(url);
+
+      setVideos(result.data);
+    }
+
+    fetchData();
+  }, [])
+
+  console.log('videos: ', videos);
+
   return (
     <div>
+
     </div>
   );
 }
