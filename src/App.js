7b004c8f20fe2919779a51f37dac0fa21e674fd7
TheRobOne <m_rusek@vp.pl>
2021-11-28 18:06:55 +0100

    Add loader

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import styles from './app.module.css';
 function App() {
   const [videos, setVideos] = useState([]);
   const [currentVideo, setCurrentVideo] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setCurrentVideo(videos[0]);
@@ -17,19 +18,30 @@ function App() {
     if (text === '') {
       return;
     } 
+    setIsLoading(true);
+
     // I am aware that it isn't secure way for passing the api key. Normally I would use some bff to hide the key and also restrict the key for the specific domain
     const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${text}&key=${process.env.REACT_APP_API_KEY}`;
     const result = await axios.get(url);
 
     setVideos(result.data?.items);
+    setIsLoading(false);
   }
 
   return (
     <div className={styles.container}>
       <TopBar search={search}/>
       <div className={styles.contentContainer}>
-        <MainArea video={currentVideo || null}/>
-        <SideBar videos={videos} setCurrentVideo={setCurrentVideo}/>
+        {
+          isLoading ? (
+            <div className={styles.loading}>Loading...</div>
+          ) : (
+            <>
+              <MainArea video={currentVideo || null}/>
+              <SideBar videos={videos} setCurrentVideo={setCurrentVideo}/>
+            </>
+          )
+        }
       </div>
     </div>
   );
